35afa39ad203547016d5202ea11b380581434684
Iyanu456 <oyerindei13@gmail.com>
2026-03-23 20:44:47 +0100

    fixing deployment bugs

--- backend/api/services/email.service.js
+++ backend/api/services/email.service.js
@@ -27,7 +27,7 @@ const getTransporter = () => {
       __transportType: "resend",
       sendMail: async ({ from, to, subject, html, text }) => {
         const response = await resend.emails.send({
-          from: env.resendEmailFrom,
+          from: `${env.resendSenderName} ${env.resendEmailFrom}`,
           to,
           subject,
           html,
